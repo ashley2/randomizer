3761ffa81ff467948cbad5bb2976b7c83036f27c
Ashley Smith <ashley22@vt.edu>
2016-02-23 19:42:23 -0800

    created basic set up css and began functionality

---
+++ main.js
@@ -0,0 +1,25 @@
+'use strict';
+
+
+document.addEventListener('DOMContentLoaded', init);
+  function init() {
+
+
+    document.getElementById('addName').onclick = function addName(){
+      console.log('click');w
+    }
+
+
+
+// document.addEventListener('click', function(displayName){
+
+ // document.getElementById("names").value = 
+
+
+// });
+}
+
+
+
+
+
